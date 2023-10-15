332f061e94dbe90783d2b7284f240973bf285489
RoyRoll <148009669+RoyRoll@users.noreply.github.com>
2023-10-16 03:27:03 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,12 @@
+# Nama: Rio Ferddinansya
+# NIM: J0403231004
+# email: rioferddinansya@gmail.com
+# function
+
+def my_function(country = "Indonesia"):
+  print("I am from " + country)
+
+my_function("Jakarta")
+my_function("Bogor")
+my_function("Bali")
+my_function()
