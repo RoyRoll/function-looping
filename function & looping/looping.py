3f86876fb7910fd380604863f27178c134882cf1
RoyRoll <148009669+RoyRoll@users.noreply.github.com>
2023-10-22 15:52:14 +0700

    Add files via upload

---
+++ function & looping/looping.py
@@ -0,0 +1,8 @@
+# Nama: Rio Ferddinansya
+# NIM: J0403231004
+# email: rioferddinansya@gmail.com
+
+days_of_week = ["Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu", "Minggu"]
+
+for day in days_of_week:
+    print(day)
